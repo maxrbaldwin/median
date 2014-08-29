6426eb1e490435335af16ec8a88a2860318c826f
maxrbaldwin <maxrbaldwin@outlook.com>
2014-08-28 22:38:36 -0400

    First model using AngularJS. Creates Google Maps URL and console.logs it

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
-var formHandler = angular.module('formHandler', []);
+angular.module('formHandler', [])
 
-function formController($scope, $http) {
-    $scope.formData = {};
-}
+.controller('formController', ['$scope', '$http', function($scope, $http){
+    //Sets the placeholder
+    $scope.text = 'Address Placeholder';
+
+    $scope.method = 'GET';
+    $scope.key1 = 'AIzaSyABzqPqDUJ_teyqNMgY1oVxyUESsvcn_so';
+
+    //ng-submit function for the address search form
+    $scope.addLookUp = function() {
+        //Gets the value of the input form
+        $scope.address = encodeURIComponent(this.address);
+        $scope.url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.address + '&key=' + $scope.key1;
+
+        console.log($scope.url);
+
+        //Resets the placeholder text back to original text
+        $scope.address = $scope.text;
+
+    };
+}]);
