b67f6cc07ced6f275a0ad400fed2b5a6f9f0b236
maxrbaldwin <maxrbaldwin@outlook.com>
2014-09-16 23:21:30 -0400

    Working seach function pulling names and images. Response is console logged in browser

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var request = require('request');
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +20,34 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.route('/addLookUp')
+    .get(function(req, res) {
 
+        var rawAddress = req.query.address,
+            address = encodeURIComponent(rawAddress),
+            method = 'GET',
+            key1 = 'AIzaSyABzqPqDUJ_teyqNMgY1oVxyUESsvcn_so',
+            key2 = 'c833fc6b098b4c2d9fbcaf1087f1ff29',
+            url1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key1
 
+        request(url1, function(error, response, geoData) {
+            var gotGeoData = JSON.parse(geoData),
+                lat = gotGeoData.results[0].geometry.location.lat,
+                long = gotGeoData.results[0].geometry.location.lng,
+                url2 = 'http://openstates.org/api/v1/legislators/geo/?lat=' + lat + '&long=' + long,
+                options = {
+                    url: url2,
+                    qs: {
+                        apikey: key2
+                    }
+                }
+
+            request(options, function(error, response, repData) {
+                var gotRepData = JSON.parse(repData);
+                res.send(gotRepData)
+            });
+        });
+    });
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,5 +80,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
+
+var server = app.listen(3000, function() {
+    console.log(server.address().port + ' is where the magic happens');
+});
 
 module.exports = app;
--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,47 @@
-angular.module('formHandler', [])
+angular.module('medianSearch', ['ngRoute'])
 
-.controller('formController', ['$scope', '$http',
-    function($scope, $http) {
-        //Sets the placeholder
-        $scope.text = 'Address Placeholder';
+.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {controller: 'formController'})
+        .when('/politician': 'profileController')
+})
 
-        var method = 'GET',
-            key1 = 'AIzaSyABzqPqDUJ_teyqNMgY1oVxyUESsvcn_so',
-            key2 = 'c833fc6b098b4c2d9fbcaf1087f1ff29';
+function formController($scope, $http) {
+    //Sets the placeholder
+    $scope.address = 'Address Placeholder';
+    $scope.searchTitle = 'Who Represents You?';
 
-        //ng-submit function for the address search form
-        $scope.addLookUp = function() {
-            //Gets the value of the input form
-            var address = encodeURIComponent(this.address),
-                url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + key1;
+    //ng-submit function for the address search form
+    $scope.addLookUp = function() {
+        //Gets the value of the input form
+        var text = this.address;
 
-            $http({
-                method: method,
-                url: url,
-                responseType: 'json'
+        console.log(text);
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/addLookUp',
+            params: {address: text}
+        })
+            .success(function(data, status) {
+                $scope.createLayout = createSearchLayout($scope, data);
             })
-                .success(function(data, status) {
-                    var lat = data.results[0].geometry.location.lat,
-                        long = data.results[0].geometry.location.lng,
-                        url2 = 'http://openstates.org/api/v1/legislators/geo/?lat=' + lat + '&long=' + long;
+            .error(function(data, status) {
 
-                    console.log(lat);
-                    console.log(long);
-                    console.log(url2);
+            });
 
-                    $http({
-                        method: method,
-                        url: url2,
-                        params: {
-                            apikey: key2
-                        }
-                    })
-                        .success(function(data, status) {
-                            console.log(data);
-                            console.log(status);
-                        })
-                        .error(function(data, status) {
-                            console.log(data, status);
-                        });
-                })
-                .error(function(data, status) {
-                    console.log(data + ' failure');
-                    console.log(status + ' failure');
-                });
+        //Resets the placeholder text back to original text
+        $scope.address = '';
 
-            //Resets the placeholder text back to original text
-            $scope.address = $scope.text;
+    };
+}
 
-        };
-    }
-]);
+function createSearchLayout($scope, data) {
+    console.log(data);
+    $scope.politicians = data;
+    $scope.searchTitle = 'These People Represent You';
+}
+
+function profileController($scope) {
+
+}
